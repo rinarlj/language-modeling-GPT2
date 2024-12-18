6f3a4dcc111640bb0aa5dbf7fe4e876d95a79c80
Rina Ralijaona <116346251+rinarlj@users.noreply.github.com>
2024-12-18 23:55:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from text_generation import generate_text
 output_dir = "./gpt2-malagasy"
 train_path, test_path = "./data/train_dataset.txt", "./data/test_dataset.txt"
 
-train_path, test_path = prepare_datasets(csv_path, train_path, test_path)
+train_path, test_path = prepare_datasets("/content/extracted/rakitra/rakitra.csv", train_path, test_path)
 
 model, tokenizer = setup_model_and_tokenizer()
 
