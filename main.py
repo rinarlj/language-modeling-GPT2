4ebd77663e4ca8321bb6f8c18bbecbd08354e5f4
Rina Ralijaona <116346251+rinarlj@users.noreply.github.com>
2024-12-19 19:17:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import os
+import wandb
 from data_processing import prepare_datasets
 from model_setup import setup_model_and_tokenizer
 from training import load_datasets, train_model
 from text_generation import generate_text
+from evaluation import evaluate_model
 
+wandb.login()
 
+wandb.init(project="gpt2-malagasy", name="fine-tuning-gpt2")
 
 output_dir = "./gpt2-malagasy"
 train_path, test_path = "train_dataset.txt", "test_dataset.txt"
@@ -16,7 +20,8 @@ model, tokenizer = setup_model_and_tokenizer()
 train_dataset, test_dataset = load_datasets(train_path, test_path, tokenizer)
 
 training_results = train_model(model, tokenizer, train_dataset, test_dataset, output_dir)
-
+metrics = evaluate_model("./gpt2-malagasy", test_dataset)
+wandb.log(metrics)
 print(training_results["metrics"])
 
 
